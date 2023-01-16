b43be91d191594e00cd6a7b90cbf6b790aafc479
yuseon <llys0331@gmail.com>
2023-01-16 22:57:25 +0900

    230116-토마토문제 복습

--- src/main/java/com/example/inflern/dfsbfs/BfsTomato.java
+++ src/main/java/com/example/inflern/dfsbfs/BfsTomato.java
@@ -5,22 +5,21 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class BfsTomato {
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int[][] board,dis;
-    static int n,m;
-    static Queue<Point> queue = new LinkedList<>();
-
-    private void bfs() {
-        while (!queue.isEmpty()) {
-            Point tmp = queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int nx = tmp.x + dx[i];
-                int ny = tmp.y + dy[i];
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 0) {
-                    board[nx][ny] = 1;
-                    queue.offer(new Point(nx, ny));
-                    dis[nx][ny] = dis[tmp.x][tmp.y] + 1;
+    static int[] dx={-1, 0, 1, 0};
+    static int[] dy={0, 1, 0, -1};
+    static int[][] board, dis;
+    static int n, m;
+    static Queue<Point> Q=new LinkedList<>();
+    public void BFS(){
+        while(!Q.isEmpty()){
+            Point tmp=Q.poll();
+            for(int i=0; i<4; i++){
+                int nx=tmp.x+dx[i];
+                int ny=tmp.y+dy[i];
+                if(nx>=0 && nx<n && ny>=0 && ny<m && board[nx][ny]==0){
+                    board[nx][ny]=1;
+                    Q.offer(new Point(nx, ny));
+                    dis[nx][ny]=dis[tmp.x][tmp.y]+1;
                 }
             }
         }
@@ -28,21 +27,18 @@ public class BfsTomato {
 
     public static void main(String[] args) {
         BfsTomato tomato = new BfsTomato();
-        Scanner scanner = new Scanner(System.in);
-        n = 4;
-        m =6;
-        board = new int[n][m];
-        dis = new int[n][m];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                board[i][j] = scanner.nextInt();
-                if(board[i][j] == 1) queue.offer(new Point(i, j));
+        Scanner kb = new Scanner(System.in);
+        m=kb.nextInt();
+        n=kb.nextInt();
+        board=new int[n][m];
+        dis=new int[n][m];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                board[i][j]=kb.nextInt();
+                if(board[i][j]==1) Q.offer(new Point(i, j));
             }
         }
-
-        tomato.bfs();
-
+        tomato.BFS();
         boolean flag=true;
         int answer=Integer.MIN_VALUE;
         for(int i=0; i<n; i++){
