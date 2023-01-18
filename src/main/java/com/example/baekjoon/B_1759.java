137543028bc993d682ee95cc69fb4d8fcf440d54
yuseon <llys0331@gmail.com>
2023-01-18 23:19:38 +0900

    230118-백준

---
+++ src/main/java/com/example/baekjoon/B_1759.java
@@ -0,0 +1,54 @@
+package com.example.baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B_1759 {
+    static char[] list, code;
+    static int L,C;
+    public static void main(String[] args) {
+        B_1759 b1759 = new B_1759();
+        Scanner scanner = new Scanner(System.in);
+        L = scanner.nextInt();
+        C = scanner.nextInt();
+        list = new char[C];
+        code = new char[L];
+        for (int i = 0; i < C; i++) {
+            list[i] = scanner.next().charAt(0);
+        }
+
+        Arrays.sort(list);
+
+        b1759.dfs(0, 0);
+    }
+
+    private void dfs(int level, int idx) {
+        if (level == L) {
+            if(valid()){
+                for(char c : code) System.out.print(c);
+                System.out.println();
+            }
+            return;
+        }else{
+            for (int i = idx; i < C; i++) {
+                code[level] = list[i];
+                dfs(level + 1, i + 1);
+            }
+        }
+    }
+
+    public boolean valid() {
+        int vowel = 0, consonant = 0;
+        for (char c : code) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                vowel++;
+            }else{
+                consonant++;
+            }
+        }
+        if (vowel >= 1 && consonant >= 2) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/example/baekjoon/B_2667.java
@@ -0,0 +1,84 @@
+package com.example.baekjoon;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Node2667 {
+    int x, y;
+
+    public Node2667(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+public class B_2667 {
+    static int[][] board, chk;
+    static int n;
+    static List<Integer> houseList = new ArrayList<>();
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {0, 1, 0, -1};
+    public static void main(String[] args) {
+        B_2667 b2667 = new B_2667();
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        board = new int[n][n];
+        chk = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            String str = scanner.next();
+            for (int j = 0; j < n; j++) {
+                board[i][j] = Integer.parseInt(String.valueOf(str.charAt(j)));
+            }
+        }
+
+        int regionCnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 1 && chk[i][j] == 0) {
+                    regionCnt++;
+                    b2667.bfs(i,j);
+                }
+            }
+        }
+
+        System.out.println(regionCnt);
+
+        Collections.sort(houseList);
+
+        for (int n : houseList) {
+            System.out.println(n);
+        }
+    }
+
+    private void bfs(int a, int b) {
+        Queue<Node2667> queue = new LinkedList<>();
+        queue.offer(new Node2667(a, b));
+        chk[a][b] = 1;
+        int houseCnt = 0;
+        while (!queue.isEmpty()) {
+            houseCnt++;
+            Node2667 temp = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = temp.x + dx[i];
+                int ny = temp.y + dy[i];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 1 && chk[nx][ny] == 0) {
+                    queue.offer(new Node2667(nx, ny));
+                    chk[nx][ny] = 1;
+                }
+            }
+        }
+        houseList.add(houseCnt);
+    }
+}
+
+//7
+//0 1 1 0 1 0 0
+//0 1 1 0 1 0 1
+//1 1 1 0 1 0 1
+//0 0 0 0 1 1 1
+//0 1 0 0 0 0 0
+//0 1 1 1 1 1 0
+//0 1 1 1 0 0 0
